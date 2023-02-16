d86420258190054aa596a70bcd2085e579d30502
Princewill Olaiya <princewillolaiya@gmail.com>
2023-02-16 11:06:25 +0100

    added main function

--- main.go
+++ main.go
@@ -2,10 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
+
+func main() {
+	var points int = 0
+	// Reading all files of directory
+	files, err := os.ReadDir("./bin/files")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Iterating through all the files
+	for _, f := range files {
+		// checking for file extensions
+		ext := filepath.Ext(f.Name())
+		// if extension relevant
+		if ext == ".pdf" {
+			//Convert to .txt
+			ExtractText(f.Name())
+			var file_name string = f.Name() + ".txt"
+
+			var text string = ReadText(file_name)
+			// checking what skills the candidate has
+			if frontEndSkills(text) {
+				points += 3
+			}
+			if backEndSkills(text) {
+				points += 1
+			}
+			if otherSkills(text) {
+				points += 1
+			}
+			fmt.Println("-------------------------------------")
+			fmt.Printf("File: %s \n", f.Name())
+			fmt.Printf("Candidate is Suitable, with score: %d/5 \n", points)
+
+		}
+	}
+}
 
 // func for checking if candidate has front end skills
 func frontEndSkills(text string) bool {
